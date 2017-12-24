bad44071869b625f6cb0aa684953a138697ec754
Ueslei de Souza <darkfrontcode@gmail.com>
2017-12-24 13:02:34 -0200

    beta@1

--- source/index.ts
+++ source/index.ts
@@ -1,34 +1,80 @@
 import * as path 				from 'path'
 import * as fs 					from 'fs'
+import * as readline			from 'readline'
+import chalk 					from 'chalk'
+
 import { NameGenerator } 		from './name-generator.class'
 import { TemplateGenerator } 	from './template-generator.class'
 
-const nameGenerator				= new NameGenerator('stylus-magazine')
-const componentTemplate 		= TemplateGenerator.component(nameGenerator)
-const controllerTemplate 		= TemplateGenerator.controller(nameGenerator)
-const pugTemplate 				= TemplateGenerator.pug(nameGenerator)
-const stylusTemplate 			= TemplateGenerator.stylus(nameGenerator)
+const log = console.log
 
-// fs.mkdirSync(dirname, (err) => {
-// 	if(err) throw err
-// 	console.log('Created!')
-// })
+const ComponentName = () => {
 
-// fs.appendFile('mynewfile1.txt', 'Hello content!', (err) =>
-// {
-// 	if (err) throw err
-// 	console.log('Saved!')
-// })
+	return new Promise((res, rej) => {
+		try
+		{
+			const rl = readline.createInterface({
+				input: process.stdin,
+				output: process.stdout
+			})
+			
+			log(chalk`{magenta Type component name:}`)
+			
+			rl.on('line', (name:string) => {
+				rl.close()
+				return res(name)
+			})
+		}
+		catch(err)
+		{
+			rej(err)
+		}
+	})
 
-// function ensureDirectoryExistence(filePath)
-// {	
-// 	const dirname = path.dirname(filePath)
+}
+
+const Next = (name:string) => {
 	
-// 	if (fs.existsSync(dirname))
-// 	{
-// 		return true
-// 	}
+	const nameGenerator				= new NameGenerator(name)
+	const componentTemplate 		= TemplateGenerator.component(nameGenerator)
+	const controllerTemplate 		= TemplateGenerator.controller(nameGenerator)
+	const pugTemplate 				= TemplateGenerator.pug(nameGenerator)
+	const stylusTemplate 			= TemplateGenerator.stylus(nameGenerator)
 
-// 	ensureDirectoryExistence(dirname)
-// 	fs.mkdirSync(dirname);
-// }
+	async function build()
+	{
+		log(chalk`{cyan {underline criating directory.. }}`)
+		await fs.mkdirSync(name)
+		log(chalk`{yellow done.}`)
+
+		log(chalk`{cyan {underline criating component entry.. }}`)
+		await fs.writeFileSync(`${name}/index.ts`, componentTemplate)
+		log(chalk`{yellow done.}`)
+
+		log(chalk`{cyan {underline criating controllers.. }}`)
+		await fs.writeFileSync(`${name}/${name}.controller.ts`, controllerTemplate)
+		log(chalk`{yellow done.}`)
+
+		log(chalk`{cyan {underline criating pug templates.. }}`)
+		await fs.writeFileSync(`${name}/${name}.template.pug`, pugTemplate)
+		log(chalk`{yellow done.}`)
+
+		log(chalk`{cyan {underline criating styles.. }}`)
+		await fs.writeFileSync(`${name}/${name}.style.styl`, stylusTemplate)
+		log(chalk`{yellow done.}`)
+
+		log(chalk`{magenta {underline ${ name } component created!}}`)
+	}
+
+	build()
+	
+}
+
+async function run()
+{
+	let name:string
+	await ComponentName().then((res:string) => name = res)
+	Next(name)
+}
+
+run()
--- source/template-generator.class.ts
+++ source/template-generator.class.ts
@@ -4,35 +4,33 @@ export class TemplateGenerator
 {	
 	public static component(nameGenerator: NameGenerator) : string
 	{
-		return `
-			import { ${ nameGenerator.controllerName } } from './${ nameGenerator.name }.controller'
-			import './${ nameGenerator.name }.style
-			const template = require('./${ nameGenerator.name }.template')
-		
-			export class ${ nameGenerator.componentName } implements ng.IComponentOptions
-			{
-				public static alias = '${ nameGenerator.aliasName }'
-				public controller: ng.Injectable<ng.IControllerConstructor>
-				public controllerAs: string
-				public template: string
-			
-				constructor() {
-					this.controller = ${ nameGenerator.controllerName }
-					this.controllerAs = "$ctrl"
-					this.template = template()
-				}
-			}
-		`
+		return `import { ${ nameGenerator.controllerName } } from './${ nameGenerator.name }.controller'
+import './${ nameGenerator.name }.style'
+const template = require('./${ nameGenerator.name }.template')
+
+export class ${ nameGenerator.componentName } implements ng.IComponentOptions
+{
+	public static alias = '${ nameGenerator.aliasName }'
+	public controller: ng.Injectable<ng.IControllerConstructor>
+	public controllerAs: string
+	public template: string
+
+	constructor() {
+		this.controller = ${ nameGenerator.controllerName }
+		this.controllerAs = "$ctrl"
+		this.template = template()
+	}
+}
+`
 	}
 
 	public static controller(nameGenerator: NameGenerator) : string
 	{
-		return `
-			export class ${ nameGenerator.controllerName } implements ng.IComponentController
-			{
-			
-			}
-		`
+		return `export class ${ nameGenerator.controllerName } implements ng.IComponentController
+{
+
+}
+`
 	}
 
 	public static pug(nameGenerator: NameGenerator) : string
@@ -42,10 +40,9 @@ export class TemplateGenerator
 
 	public static stylus(nameGenerator: NameGenerator) : string
 	{
-		return `
-/* ==========================================================================
+		return `/* ==========================================================================
 -- ${ nameGenerator.captalizeName } Component
 ========================================================================== */
-   		`
+`
 	}
 }
